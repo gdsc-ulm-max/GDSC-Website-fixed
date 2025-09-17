7e0970e052f6082c62a65804231661ea07fefe4d
Prasanna Jha <prasannajha401@gmail.com>
2025-09-16 23:53:06 -0500

    Removed Hawkathon tab from the navigation

--- src/NavBar.js
+++ src/NavBar.js
@@ -35,7 +35,7 @@ function NavBar() {
         <Link to="/events" onClick={closeMenu}>Events</Link>
         <Link to="/gallery" onClick={closeMenu}>Gallery</Link>
         <Link to="/code-clash" onClick={closeMenu}>CodeClash</Link>
-        <Link to="/hawkathon" onClick={closeMenu}>Hawkathon</Link>
+        {/*<Link to="/hawkathon" onClick={closeMenu}>Hawkathon</Link>*/}
         <Link to="/forms" onClick={closeMenu}>Forms</Link>
         <Link to="/join" onClick={closeMenu}>Join</Link>
         <Link to="/about" onClick={closeMenu}>About</Link>
